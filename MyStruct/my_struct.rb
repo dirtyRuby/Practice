7586d8888aed7f8d1c0ef2017a6094eca36f3f7a
dirtyRuby <Dirty-Glamer@yandex.ru>
2014-10-31 00:45:11 +0200

    30.10.2014

---
+++ MyStruct/my_struct.rb
@@ -0,0 +1,51 @@
+#Вариант1
+class MyStruct
+  def self.new(*fields)
+    c = Class.new do
+      fields.each do |field|
+
+        define_method field.to_sym do
+          instance_variable_get("@#{field}")
+        end
+
+        define_method "#{field}=".to_sym do |arg|
+          instance_variable_set("@#{field}", arg)
+        end
+
+        define_method :initialize do |*values|
+          fields.each_with_index do |name,i|
+            instance_variable_set("@#{name}", values[i])
+          end
+        end
+      end
+    end
+  end
+end
+
+#Вариант2
+class MyStruct
+  def self.new *args
+    c = Class.new
+
+    args.each do |meth_name|
+      c.class_eval %Q{
+        def #{meth_name}(param = nil)
+          if param
+            @#{meth_name} = param
+          else
+            @#{meth_name}
+          end
+        end
+      }
+    end
+
+    c.class_eval %Q{
+      def initialize(*arg)
+        #{args}.each_with_index do |attr, i|
+          instance_variable_set("@" + attr.to_s, arg[i])
+        end
+      end
+    }
+    c
+  end
+end
