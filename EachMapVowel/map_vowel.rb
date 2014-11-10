c7db9ece1476b69260aeeb891fe6eaf401fa6209
dirtyRuby <Dirty-Glamer@yandex.ru>
2014-11-10 14:13:41 +0200

    Update map_vowel.rb

--- EachMapVowel/map_vowel.rb
+++ EachMapVowel/map_vowel.rb
@@ -1,10 +1,10 @@
 module MapVowel
   def map_vowel(&proc)
-    vowels = ['a','o','u','i','e']
+    vowels = ['a','o','u','i','e'].to_enum
     vowels.map{proc.call(vowels.next)}
   end
 end
 #Test
 include MapVowel
 
-map_vowel{ |x| puts x}
+map_vowel{ |x| puts x+'1'}
