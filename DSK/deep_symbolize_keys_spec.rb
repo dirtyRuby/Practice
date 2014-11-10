f28234e80e2800af09bfe8b4a4f882858968e2d1
dirtyRuby <Dirty-Glamer@yandex.ru>
2014-11-10 16:01:20 +0200

    Update deep_symbolize_keys_spec.rb

--- DSK/deep_symbolize_keys_spec.rb
+++ DSK/deep_symbolize_keys_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 require 'deep_symbolize_keys'
 
 describe Hash do
-
-  it 'Should return object of Hash' do
-    hash = {'fred' => {
+  before :each do
+    @hash = {'fred' => {
         'name' => 'Fred Elliott',
         'age' => 63,
         'gender' => 'male',
@@ -17,40 +16,17 @@ describe Hash do
                 'gender' => 'female'
             }
     }
-    hash.deep_symbolize_keys!.class.should == Hash
+  end
+  it 'Should return object of Hash' do
+    @hash.deep_symbolize_keys!.class.should == Hash
   end
 
   it 'Should return same object that call deep_symbolize_keys method' do
-    hash = {'fred' => {
-        'name' => 'Fred Elliott',
-        'age' => 63,
-        'gender' => 'male',
-        'favorite painters' => ['Monet', 'Constable', 'Da Vinci']
-    },
-            'janet' => {
-                'name' => 'Janet S Porter',
-                'age' => 55,
-                'gender' => 'female'
-            }
-    }
-    id = hash.object_id
-    hash.deep_symbolize_keys!.object_id.should == id
+    id = @hash.object_id
+    @hash.deep_symbolize_keys!.object_id.should == id
   end
 
   it 'Should turn all keys of String type into Symbols at all levels of Hash nesting' do
-    hash = {'fred' => {
-        'name' => 'Fred Elliott',
-        'age' => 63,
-        'gender' => 'male',
-        'favorite painters' => ['Monet', 'Constable', 'Da Vinci']
-    },
-            'janet' => {
-                'name' => 'Janet S Porter',
-                'age' => 55,
-                'gender' => 'female'
-            }
-    }
-
     def deep_test_func (hash)
       hash.each{ |k,v|
         k.class.should == Symbol
@@ -59,6 +35,6 @@ describe Hash do
         end
       }
     end
-    deep_test_func(hash.deep_symbolize_keys!)
+    deep_test_func(@hash.deep_symbolize_keys!)
   end
 end
