94b73a48b06703fc50e4c0d08cb86f8927387532
dirtyRuby <Dirty-Glamer@yandex.ru>
2014-11-14 00:11:21 +0200

    Update deep_symbolize_keys.rb

--- DSK/spec/deep_symbolize_keys.rb
+++ DSK/spec/deep_symbolize_keys.rb
@@ -1,26 +1,16 @@
 class Hash
- private
-  def perform(hash)
-    raise ArgumentError, "Wrong argument type. Hash expected when #{hash.class} given." if hash.class != Hash
-    new_hash = {}
-    hash.each { |k,v|
-        key = k.sub(" ","_")
-      if v.class == Hash
-        new_hash[key.to_sym] = perform(v)
-      else
-        new_hash[key.to_sym] = v
-      end
-    }
-    hash.clear
-    new_hash.each{ |k,v|
-      hash[k] = v
-    }
-    hash
-  end
-
-  public
   def deep_symbolize_keys!
-    perform (self)
+    n = {}
+    self.each do |k,v|
+      if v.is_a?(Hash)
+        n[k.to_sym] = v.deep_symbolize_keys!
+      else
+        n[k.to_sym] = v
+      end
+    end
+    self.clear
+    n.each {|k,v| self[k] = v}
+    self
   end
 end
 people = {'fred' => {
@@ -28,13 +18,11 @@ people = {'fred' => {
     'age' => 63,
     'gender' => 'male',
     'favorite painters' => ['Monet', 'Constable', 'Da Vinci']
-},
-        'janet' => {
-            'name' => 'Janet S Porter',
-            'age' => 55,
-            'gender' => 'female'
-        }
+  },
+  'janet' => {
+    'name' => 'Janet S Porter',
+    'age' => 55,
+    'gender' => 'female'
+  }
 }
 puts people.deep_symbolize_keys!
-
-
