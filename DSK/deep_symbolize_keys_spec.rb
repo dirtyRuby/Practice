75abd66ae42faec1bc4a73239b76a60df5d32cb6
dirtyRuby <Dirty-Glamer@yandex.ru>
2014-11-14 00:10:28 +0200

    Update deep_symbolize_keys_spec.rb

--- DSK/deep_symbolize_keys_spec.rb
+++ DSK/deep_symbolize_keys_spec.rb
@@ -9,31 +9,30 @@ describe Hash do
         'age' => 63,
         'gender' => 'male',
         'favorite painters' => ['Monet', 'Constable', 'Da Vinci']
-    },
-            'janet' => {
-                'name' => 'Janet S Porter',
-                'age' => 55,
-                'gender' => 'female'
-            }
+      },
+      'janet' => {
+        'name' => 'Janet S Porter',
+        'age' => 55,
+        'gender' => 'female'
+      }
     }
   end
+
   it 'Should return object of Hash' do
-    @hash.deep_symbolize_keys!.class.should == Hash
+    expect(@hash.deep_symbolize_keys!.class).to eq(Hash)
   end
 
   it 'Should return same object that call deep_symbolize_keys method' do
     id = @hash.object_id
-    @hash.deep_symbolize_keys!.object_id.should == id
+    expect(@hash.deep_symbolize_keys!.object_id).to eq(id)
   end
 
-  it 'Should turn all keys of String type into Symbols at all levels of Hash nesting' do
+  it 'All hash keys should be symbols' do
     def deep_test_func (hash)
-      hash.each{ |k,v|
-        k.class.should == Symbol
-        if v.class == Hash
-          deep_test_func(v)
-        end
-      }
+      hash.each do |k,v|
+        expect(k.class).to eq(Symbol)
+        deep_test_func(v) if v.is_a?(Hash)
+      end
     end
     deep_test_func(@hash.deep_symbolize_keys!)
   end
